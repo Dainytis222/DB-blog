2d08bcec34264be48bd5a95fb55c40f1803fc35d
Dainius Balsys <dainytis@gmail.com>
2022-04-24 15:29:42 +0300

    Import Application Context

--- src/main/java/dainius/app/Main.java
+++ src/main/java/dainius/app/Main.java
@@ -1,9 +1,13 @@
 package dainius.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+
+    ApplicationContext context = new AnnotationConfigApplicationContext("dainius.app");
   }
 
 }
